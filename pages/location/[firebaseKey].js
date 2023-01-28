64b35d573fa49a09770a61554d077f2ea40f371b
Allison Blumenthal <blumen.allison@gmail.com>
2023-01-28 10:52:00 -0500

    added styling to location pages

--- pages/location/[firebaseKey].js
+++ pages/location/[firebaseKey].js
@@ -20,20 +20,30 @@ export default function ViewLocation() {
 
   return (
     <>
-      <div className="mt-5 d-flex flex-wrap">
-        <div className="d-flex flex-column">
-          <img src={locationDetails.location_image} alt={locationDetails.location_name} style={{ width: '300px' }} />
-        </div>
-        <div className="text-white ms-5 details">
-          <h2>
-            {locationDetails.location_name}
-          </h2>
-        </div>
-        <div className="d-flex flex-wrap">
-          {locationDetails.dogs?.map((dog) => (
-            <DogCard key={dog.firebaseKey} dogObj={dog} onUpdate={getLocationDetails} />
-          ))}
-        </div>
+      <div className="text-white ms-5 details">
+        <h2>
+          {locationDetails.location_name}
+        </h2>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <img src={locationDetails.location_image} alt={locationDetails.location_name} style={{ width: '300px' }} />
+      </div>
+      <div
+        className="d-flex flex-wrap"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '20px',
+        }}
+      >
+        {locationDetails.dogs?.map((dog) => (
+          <DogCard key={dog.firebaseKey} dogObj={dog} onUpdate={getLocationDetails} />
+        ))}
       </div>
     </>
   );
